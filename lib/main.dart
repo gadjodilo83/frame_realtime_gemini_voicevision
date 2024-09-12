be7a815c09308ebdc1d28564ecf64e11eb300148
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-09-12 10:44:21 +1000

    added simple_frame_app package dependency

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 import 'package:record/record.dart';
 import 'package:vosk_flutter/vosk_flutter.dart';
-
-import 'frame_helper.dart';
-import 'simple_frame_app.dart';
+import 'package:simple_frame_app/frame_helper.dart';
+import 'package:simple_frame_app/simple_frame_app.dart';
 
 void main() => runApp(const MainApp());
 
